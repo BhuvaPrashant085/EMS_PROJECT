1b28ed56174d5ad3d0ec917e6f7257bad2804d06
bhuva_prashant_085 <152417485+BhuvaPrashant085@users.noreply.github.com>
2026-07-27 10:56:47 +0530

    xgvpdfdklgxoofuz

--- EMS/settings.py
+++ EMS/settings.py
@@ -26,7 +26,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'prashantbhuva085@gmail.com'
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_PASSWORD = 'xgvpdfdklgxoofuz'
 
 import os
 
