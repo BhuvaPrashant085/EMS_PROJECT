22bae371cea664a8c4c3fde7c260863892d8fee5
bhuva_prashant_085 <152417485+BhuvaPrashant085@users.noreply.github.com>
2026-07-27 11:38:29 +0530

    Fix signup path indentation in urls.py

--- users/urls.py
+++ users/urls.py
@@ -4,8 +4,8 @@ from . import views
 urlpatterns = [
 
     path('', views.home, name='home'),
-    path('signup/', views.signup, name='signup'),
     path('login/', views.login_user, name='login'),
+     path('signup/', views.signup, name='signup'),
     path('logout/', views.logout_user, name='logout'),
     path('citizen/', views.citizen_dashboard, name='citizen_dashboard'),
     path('operator/', views.operator_dashboard, name='operator_dashboard'),
