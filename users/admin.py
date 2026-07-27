d447e85ef62a8f19a7b165b1e857c39fe1bf2cd4
bhuva_prashant_085 <152417485+BhuvaPrashant085@users.noreply.github.com>
2026-07-27 11:40:02 +0530

    Register Ambulance and FireStation in admin

--- users/admin.py
+++ users/admin.py
@@ -3,10 +3,13 @@ from .models import *
 
 admin.site.register(Citizen)
 admin.site.register(Emergency)
+
 admin.site.register(Ambulance)
 admin.site.register(Hospital)
 admin.site.register(PoliceStation)
+
 admin.site.register(FireStation)
-admin.site.register(EmergencyAssignment)
 admin.site.register(EmergencyLog)
+admin.site.register(EmergencyAssignment)
+
 admin.site.register(TrustedContact)
