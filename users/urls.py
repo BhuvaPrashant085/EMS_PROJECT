c2e420faa76d0e21f4baadcc3b0b1c57020e955c
bhuva_prashant_085 <152417485+BhuvaPrashant085@users.noreply.github.com>
2026-07-27 11:39:05 +0530

    Update urls.py

--- users/urls.py
+++ users/urls.py
@@ -10,15 +10,15 @@ urlpatterns = [
     path('logout/', views.logout_user, name='logout'),
     path('admin-dashboard/', views.admin_dashboard, name='admin_dashboard'),
     path('citizen/', views.citizen_dashboard, name='citizen_dashboard'),
-    path('operator/', views.operator_dashboard, name='operator_dashboard'),
-    
+      
+ 
+       path('operator/', views.operator_dashboard, name='operator_dashboard'),
     path('send_emergency/', views.send_emergency, name='send_emergency'),
     path('profile/', views.profile, name='profile'),
     path('approve_emergency/<int:emergency_id>/', views.approve_emergency, name='approve_emergency'),
     path("hospital-dashboard/", views.hospital_dashboard, name="hospital_dashboard"),
 path("police-dashboard/", views.police_dashboard, name="police_dashboard"),
 path("fire-dashboard/", views.fire_dashboard, name="fire_dashboard"),
-
 path(
 "accept-emergency/<int:emergency_id>/<str:service_type>/",
 views.accept_emergency,
