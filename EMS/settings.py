15d740d095b1c94fca6a4eed29a9e1c8c1d734b0
bhuva_prashant_085 <152417485+BhuvaPrashant085@users.noreply.github.com>
2026-08-05 20:33:48 +0530

    Update settings.py

--- EMS/settings.py
+++ EMS/settings.py
@@ -51,7 +51,7 @@ TWILIO_WHATSAPP_NUMBER = os.getenv("TWILIO_WHATSAPP_NUMBER", "")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["4.vercel.app"]
+ALLOWED_HOSTS = ["5.vercel.app"]
 
 
 
