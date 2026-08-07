2917d5bbfb457452535d9033e6d1925d40c5d9eb
bhuva_prashant_085 <152417485+BhuvaPrashant085@users.noreply.github.com>
2026-08-07 20:00:57 +0530

    Update ALLOWED_HOSTS for development environment

--- EMS/settings.py
+++ EMS/settings.py
@@ -51,7 +51,7 @@ TWILIO_WHATSAPP_NUMBER = os.getenv("TWILIO_WHATSAPP_NUMBER", "")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [""]
+ALLOWED_HOSTS = ["prashantbhuva"]
 
 
 
